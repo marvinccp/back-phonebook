0c640fe5ee286c0bd8d5ab8482c71defab838299
[Marvin] <[marvinccp@gmail.com]>
2022-11-11 01:51:26 +0100

    key fix

--- src/utils/auth/strategies/jwt.strategy.js
+++ src/utils/auth/strategies/jwt.strategy.js
@@ -1,12 +1,14 @@
 const { Strategy, ExtractJwt } = require('passport-jwt')
-const { config } = require('../../../config')
+// const { config } = require('../../../config')
+
+
+let key = 'D743DA5D6657E6A88B22FCB3D3FE9';
 
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.jwtSecret
-}
-console.log(config.jwtSecret);
+  secretOrKey: key,
+};
 
 const jwtStrategy = new Strategy(options, (payload, done)=>{
   return done(null, payload)
